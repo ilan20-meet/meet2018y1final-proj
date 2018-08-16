7e88a1c99a8b7d25d81f66c15bd19ca66b2aad77
ilan20-meet <ilan20@meet.mit.edu>
2018-08-16 09:26:04 -0400

    Stopping here

--- FallingTrash.py
+++ FallingTrash.py
@@ -1,14 +1,17 @@
-import turtle
+i mport turtle
 import random
 import time
 
+
+
 turtle.bgpic("env.gif")
+
 score = 0
 scoreTurtle=turtle.clone()
 scoreTurtle.hideturtle()
 scoreTurtle.penup()
 scoreTurtle.goto(480,450)
-scoreTurtle.write(int(score),align="center",font=("times",33,"bold"))
+scoreTurtle.write(score,align="center",font=("times",33,"bold"))
 
 game_over=turtle.clone()
 game_over.hideturtle()
@@ -17,13 +20,12 @@ game_over.goto(0,0)
 
 
 
-turtle.register_shape("bottle.gif")
+
 
 SQUARE_SIZE = 20
 
 man=turtle.clone()
 turtle.hideturtle()
-
 turtle.register_shape("man.gif")
 man.shape("man.gif")
 turtle.setup(1000,1000)
@@ -31,6 +33,7 @@ man.penup()
 man.goto(0,-150)
 
 turtle.tracer(1,0)
+
 LEFT_ARROW = "Left" 
 RIGHT_ARROW = "Right" 
 LEFT=1
@@ -48,48 +51,38 @@ fire.penup()
 fire.showturtle()
 fire.goto(0,-490)
 
-
+turtle.register_shape("bottle.gif")
 
 gun1 = turtle.clone()
-#gun2 = turtle.clone()
-
-#gun3 = turtle.clone()
-#gun4 = turtle.clone()
-#gun5 = turtle.clone()
-#gun6 = turtle.clone()
-#gun7 = turtle.clone()
-#gun8 = turtle.clone()
-index = 0
 
 
-gun_list = [gun1]
+gun1.hideturtle()
+gun1.penup()
+
+
 
 SPACE = "space"
-
+#a function that creates a "gun"
 def up():
-    global index
-    index+=1
+    
     print('You shoot the gun!')
-    gun1.speed(1)
+    
     turtle.register_shape("recycle.gif")
     gun1.shape("recycle.gif")
     gun1.hideturtle()
     gun1.penup()
     gun1.setheading(90)
-    #gun_list[index%len(gun_list)].left(90)
-    # x = snake.pos()[0]
-    
-    #gun_list[index%len(gun_list)].goto(x,1000)
     gun1.goto(man.pos())
     gun1.showturtle()
     gun1.forward(1000)
-    
+
 z=0
 difficulty=0.1
 num_bottle=0
 max_bottle=8
 bottle_list=[]
-def make_bottle():  #need to make a few at a time
+#a function that creates bottles
+def make_bottle(): 
     for num_bottle in range(max_bottle):
         random_x=random.randint(-470,470)
         bottle=turtle.clone()
@@ -100,39 +93,38 @@ def make_bottle():  #need to make a few at a time
         bottle.goto(random_x,480)
         bottle_list.append(bottle)
         bottle.showturtle()
-        bottle.speed(10)
+        bottle.speed(0)
     
-    
+ #a function that chooses a random bottle and moves it by a little bit   
 def move_bottle():
     random_bottle=random.randint(0,max_bottle-1)
     bottle=bottle_list[random_bottle]
-    bottle.goto(bottle.pos()[0],bottle.pos()[1]- SQUARE_SIZE)
+    bottle.goto(bottle.pos()[0],bottle.pos()[1]- SQUARE_SIZE)  
     time.sleep(difficulty)        
-
+ 
 old_stamp="not set"
 
-
+#a function that enables the movement of the main character
 def move_man():
     my_pos = man.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
-    #global old_stamp
+    
     if direction==RIGHT:
         man.goto(x_pos + SQUARE_SIZE, y_pos)
         print("You moved right!")
     elif direction==LEFT:
         man.goto(x_pos - SQUARE_SIZE, y_pos)
         print("You moved left!")
-    #print(old_stamp)    
-    #old_stamp.clearstamp()
-    #old_stamp=man.stamp()
+    
         
-
+#defining movement to the left
 def left():
     global direction 
     direction=LEFT 
     print("You pressed the left key!")
     move_man()
+#defining movement to the right
 def right():
     global direction 
     direction=RIGHT 
@@ -144,20 +136,25 @@ turtle.onkeypress(right,RIGHT_ARROW) # Create listener for up key
 turtle.onkeypress(left,LEFT_ARROW) # Create listener for up key
 turtle.listen()
 make_bottle()
+#this is our main loop which always runs
 while True:
     
    
     turtle.onkeypress(up,SPACE)
+    #each time we choose a bottle and check for it several things
     for bottle in bottle_list:
+        #here we check if a bottle hits the bottom of the screen, if so you lose the game.
         if  bottle.pos()[1] < -410:
-            game_over.write("Great Job ! You Recycle " + str(score) + " bottles",align="center",font=("times",33,"bold"))
+            game_over.write("Great Job ! You Recycle " + str(score) + " bottles",align="center",font=("times",45,"bold"))
             quit()
-        if  gun1.pos()[0] < bottle.pos()[0] + 30 and gun1.pos()[0] > bottle.pos()[0] - 30 and gun1.pos()[1] > bottle.pos()[1] - 30:
+        #here we check if the bullet hits a bottle, if so the bottle disappears
+        if  gun1.pos()[0] < bottle.pos()[0] + 30 and gun1.pos()[0] > bottle.pos()[0] - 30 and gun1.pos()[1] > bottle.pos()[1] - 30 :
             bottle_list.remove(bottle)
             bottle.hideturtle()
             score+=1
             scoreTurtle.clear()
             scoreTurtle.write(int(score),align="center",font=("times",33,"bold"))
+        #here we check if there are less than 8 bottles, if so we create another one
         if len(bottle_list) < max_bottle:
             random_x=random.randint(-470,470)
             bottle=turtle.clone()
@@ -169,6 +166,7 @@ while True:
             bottle_list.append(bottle)
             bottle.showturtle()
             bottle.speed(8)    
+        #here we make the bottles move faster every 10 points you get
         if score - z >= 10:
             difficulty-=0.025
             z=z+10
